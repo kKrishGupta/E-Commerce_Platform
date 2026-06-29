9b3f0fafe65e6aab0f2c8e21a305133810f94802
kKrishGupta <kg20060220@gmail.com>
2026-06-29 16:54:42 +0530

    ckoudinary cofiguration set up

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const userRoutes = require('./routes/auth.routes');
+const productRoutes = require('./routes/product.routes');
+const orderRoutes = require('./routes/order.routes');
+const paymentRoutes = require('./routes/payment.routes');
+const analyticsRoutes = require('./routes/analytics.routes');
+const reportRoutes = require('./routes/report.routes');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,5 +16,10 @@ app.get("/", (req, res) => {
     res.send("Server is running...");
 });
 app.use('/api/auth', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/payments', paymentRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/reports', reportRoutes);
 
 module.exports = app;
